e074efa509e79ea33a2b071369d96a98ecaf183d
rc678 <rosheenmail@gmail.com>
2014-10-05 01:29:56 -0400

    skeleton code for 1st half
    if you don't like it, we can revert back to the previous commit. I also suggest we create a peer class

--- part1/RUBTClient.java
+++ part1/RUBTClient.java
@@ -36,7 +36,7 @@ public class RUBTClient {
 			System.err.println("torrent file cannot be null");
 			return;
 		}
-        
+        /*prints out the torrent file information. fields are the key value pairs of anounnce, info_hash, etc*/
         ToolKit.print(data_in_torrent.torrent_file_map);
         sendRequestToTracker(data_in_torrent);
 		
@@ -68,26 +68,48 @@ public class RUBTClient {
 
         }/*end of torrentParser method*/
 
-    /*sends an HTTP GET request to the tracker and creates connection*/
+    /*This was really helpful for learning about torrents/bittorrent clients
+      https://wiki.theory.org/BitTorrentSpecification*/
+    /*sends an HTTP GET request to the tracker and creates connection.*/
     private static void sendRequestToTracker(TorrentInfo torrentInfo){
         /*information needed to communicate with the trackers. need to be escaped, write method for that later*/
         /*also not sure if port, uploaded, and downloaded are strings. need to figure out later*/
-        String info_hash, peer_id, left, event, port, uploaded, downloaded;
+        String info_hash, peer_id, port, uploaded, downloaded, left, event;
+        String oldURL = torrentInfo.announce_url.toString();
+        
+        /*steps:
+        * 1. convert the info_hash from the torrentInfo into hex (escaping)
+        * 2. get port number from info_hash, you can check to see if it's correct with the announce url
+        * 3. figure out how to use java.net.url class to send HTTP request
+        * 4. all the tracker request parameters are the above declared variables
+        * 5. calls captureTrackerResponse to store the data in a variable
+        */
         HttpURLConnection connection_to_tracker;
         
     }/*end of sendRequest method*/
     
-    /*gets IP address of tracker from torrent*/
-    private static void extractIP(TorrentInfo torrentInfo){
-        
+    /*Converts the info_hash dictionary into hex. This is decoding the SHA1*/
+    private static void escape(byte[] torrent_info){
+        /*need to escape the data from the info_hash*/
     }
     
-    /*gets port number of tracker for torrent*/
-    private static void extractPort(TorrentInfo torrentInfo){
-        
-    }
-    private static void captureTrackerResponse(){
+    /*gets the peer list and other info from the tracker*/
+    private static byte[] captureTrackerResponse(String URL){
+        /* 1. use URL to make a HTTPURLConnection to tracker
+        *  2. store peer list from tracker. the response is a bencoded dictionary
+        */
+        byte[] trackerResponse; 
+        return null;
         
     }/*end of captureTrackerResponse*/
-
+    
+    /*the tracker responds with a bencoded dictionary as the peer list*/
+    /*uses the becoder2 class to decode the dictorary and store the peers in a list*/
+    private static void decodeTrackerResponse(byte[] tracker_response){
+        /*
+            1. store the interval and peers as a list (Have a peer object with peerid, ip, port)
+            2. Make Object o and call beconder2.decode() with tracker response
+            3. You now have all the necessary info for a handshake
+        */
+    }
 }
