4ad811402908956241a0897a6c4eb9440a90f89a
rc678 <rosheenmail@gmail.com>
2014-11-09 14:58:34 -0500

    original

---
+++ part3/HaveMessage.java
@@ -0,0 +1,29 @@
+/**
+* @author Julie Duncan
+* @author David Rubin
+* @author Rosheen Chaudhry
+*/
+
+public class HaveMessage extends Message{
+	
+	/* The index for the piece */
+	public int piece_index;
+	
+	/**
+	 * The ‘Have’ message consists of a 4-byte length prefix and single byte message id, 
+	 * followed by a 4-byte payload representing the piece number (0-based index).
+	 * @param piece_index
+	 */
+	public HaveMessage(int piece_index){
+		super(5, Message.have);
+		this.piece_index = piece_index;
+	}
+	
+	/**
+	 * @return the index of the piece.
+	 */
+	public int getPieceIndex() {
+		return piece_index;
+	}
+	
+}
