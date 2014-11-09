088aff75e28cd5c79c027cc90002976497cd5b99
rc678 <rosheenmail@gmail.com>
2014-11-09 14:58:05 -0500

    Create BitfieldMessage.java

---
+++ part3/BitfieldMessage.java
@@ -0,0 +1,29 @@
+/**
+* @author Julie Duncan
+* @author David Rubin
+* @author Rosheen Chaudhry
+*/
+
+public class BitfieldMessage extends Message{
+
+	/*The bitfield message is variable length, where X is the length of the bitfield. 
+	 * The payload is a bitfield representing the pieces that have been successfully downloaded. 
+	 * The high bit in the first byte corresponds to piece index 0. Bits that are cleared 
+	 * indicate a missing piece, and set bits indicate a valid and available piece. Spare 
+	 * bits at the end are set to zero.
+	*/
+	private byte[] pieces;
+	
+	public BitfieldMessage(int length, byte[] pieces){
+		super(length, Message.bitfield);
+		this.pieces = pieces;
+	}
+/**
+ * 
+ * * @return a byte array of the pieces the peer has.
+ */	
+	public byte[] getPieces(){
+		return this.pieces;
+	}
+	
+}
