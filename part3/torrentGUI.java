da06b813cd0aa4b1ab770642e9a86bc8edc2f452
rc678 <rosheenmail@gmail.com>
2014-11-23 23:10:46 -0500

    added labels for display on GUI

--- part3/torrentGUI.java
+++ part3/torrentGUI.java
@@ -1,19 +1,20 @@
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextField;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rosheen
+ * @author Rosheen C.
+ * @author David R.
+ * @author Julie D.
  */
 public class torrentGUI extends javax.swing.JFrame {
 
-	public boolean programStarted = false;
+    public boolean programStarted = false; 
+    
+    static int downloadedPieces = 0; 
+    
     /**
      * Creates new form torrentGUI
      */
@@ -33,30 +34,19 @@ public class torrentGUI extends javax.swing.JFrame {
         startButton = new javax.swing.JButton();
         stopButton = new javax.swing.JButton();
         clearButton = new javax.swing.JButton();
+        downloadLabel = new javax.swing.JLabel();
+        uploadLabel = new javax.swing.JLabel();
+        downloadDisplay = new javax.swing.JTextField();
+        uploadDisplay = new javax.swing.JTextField();
+        timeLabel = new javax.swing.JLabel();
+        timeDisplay = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         startButton.setText("START");
         startButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                try {
-					startButtonActionPerformed(evt);
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (NullPointerException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (BencodingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+                startButtonActionPerformed(evt);
             }
         });
 
@@ -74,6 +64,18 @@ public class torrentGUI extends javax.swing.JFrame {
             }
         });
 
+        downloadLabel.setText("Downloaded:");
+
+        uploadLabel.setText("Uploaded:");
+
+        downloadDisplay.setText("0");
+
+        uploadDisplay.setText("0");
+
+        timeLabel.setText("Time: ");
+
+        timeDisplay.setText("0");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -84,51 +86,96 @@ public class torrentGUI extends javax.swing.JFrame {
                     .addComponent(clearButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(stopButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(startButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(332, Short.MAX_VALUE))
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(timeLabel)
+                    .addComponent(downloadLabel)
+                    .addComponent(uploadLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(timeDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(downloadDisplay, javax.swing.GroupLayout.DEFAULT_SIZE, 79, Short.MAX_VALUE)
+                        .addComponent(uploadDisplay)))
+                .addContainerGap(31, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(startButton)
-                .addGap(18, 18, 18)
-                .addComponent(stopButton)
-                .addGap(18, 18, 18)
-                .addComponent(clearButton)
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(startButton)
+                    .addComponent(uploadLabel)
+                    .addComponent(uploadDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(stopButton)
+                    .addComponent(downloadLabel)
+                    .addComponent(downloadDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(clearButton)
+                    .addComponent(timeLabel)
+                    .addComponent(timeDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>                        
 
-    private void startButtonActionPerformed(java.awt.event.ActionEvent evt)throws UnknownHostException, IOException, NullPointerException, BencodingException, InterruptedException  {                                            
-        // TODO add your handling code here:
-       if(!programStarted){
-    	RUBTClient.begin();
+    private void startButtonActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        if(!programStarted){
+            try {
+                RUBTClient.begin();
+            } catch (IOException ex) {
+                Logger.getLogger(torrentGUI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NullPointerException ex) {
+                Logger.getLogger(torrentGUI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (BencodingException ex) {
+                Logger.getLogger(torrentGUI.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(torrentGUI.class.getName()).log(Level.SEVERE, null, ex);
+            }
         startButton.setEnabled(false);
         programStarted = true;
        }
     }                                           
 
     private void stopButtonActionPerformed(java.awt.event.ActionEvent evt) {                                           
-        // TODO add your handling code here:
-        if(programStarted){
+
+          if(programStarted){
         	for(int i = 0; i < RUBTClient.connectedPeers.size(); i++)
         	{
         		RUBTClient.connectedPeers.get(i).th.stop();
         		stopButton.setEnabled(false);
         	}
+                updateTime();
         }
     }                                          
 
     private void clearButtonActionPerformed(java.awt.event.ActionEvent evt) {                                            
-        // TODO add your handling code here:
+        uploadDisplay.setText("0");
+        downloadDisplay.setText("0");
+        timeDisplay.setText("0");
     }                                           
 
+    public static void updateDownload(){
+        String display = Integer.toString(RUBTClient.downloaded);
+        downloadDisplay.setText(display);
+    }
+    
+    public static void updateUpload(){
+        String display = Integer.toString(RUBTClient.uploaded);
+        uploadDisplay.setText(display);
+    }
+    
+    public static void updateTime(){
+        String display = Double.toString(RUBTClient.downloadTime);
+        timeDisplay.setText(display);
+    }
     /**
      * @param args the command line arguments
      */
-    
     public static void startGUI() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -154,19 +201,22 @@ public class torrentGUI extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new torrentGUI().setVisible(true);
             }
         });
     }//end of main
-     
 
-   
     // Variables declaration - do not modify                     
     private javax.swing.JButton clearButton;
+    public static javax.swing.JTextField downloadDisplay;
+    private javax.swing.JLabel downloadLabel;
     private javax.swing.JButton startButton;
     private javax.swing.JButton stopButton;
+    public static javax.swing.JTextField timeDisplay;
+    private javax.swing.JLabel timeLabel;
+    public static javax.swing.JTextField uploadDisplay;
+    private javax.swing.JLabel uploadLabel;
     // End of variables declaration                   
 }
